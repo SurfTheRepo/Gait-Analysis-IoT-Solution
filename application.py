aa63525c04d6112a1a9fa8cbc3d4c5f1acf1282f
Mike Gysel <z5251938@ad.unsw.edu.au>
2020-11-22 20:47:48 +1100

    Updated project homepage

--- application.py
+++ application.py
@@ -20,6 +20,9 @@ cursor = db.cursor()
 sql = '''use gait'''
 cursor.execute(sql)
 
+'''
+Insert details into user gait database
+'''
 def insert_details(ip_address,data):
     sql = '''
     insert into user_gait_data(ip_address, data) values('%s', '%s')
@@ -27,14 +30,29 @@ def insert_details(ip_address,data):
     cursor.execute(sql)
     db.commit()
 
+'''
+Get details from user gait database
+'''
 def get_details():
     sql = '''select * from user_gait_data'''
     cursor.execute(sql)
     details = cursor.fetchall()
     return details
 
-@application.route('/', methods=['GET', 'POST'])
-def index():
+'''
+Gives last gait verification value
+'''
+def get_last_detail():
+    sql = '''SELECT * FROM user_gait_data ORDER BY ID DESC LIMIT 1'''
+    cursor.execute(sql)
+    details = cursor.fetchall()
+    return details[0][2]
+
+'''
+Page to display data in database
+'''
+@application.route('/data', methods=['GET', 'POST'])
+def data():
     if request.method == 'GET':
         details = get_details()
         return render_template('index.html', var=details)
@@ -62,28 +80,14 @@ def index():
         print(details)
         return render_template('index.html',var=details)
 
+'''
+Application homepage
+'''
 @application.route('/homepage', methods=['GET'])
 def homepage():
     if request.method == 'GET':
         details = get_details()
         return render_template('homepage.html', var=details)
-
-'''
-Displays input data received
-Used to test that data can be sent from SensorTag
-'''
-'''
-@application.route('/inputData', methods=['POST'])
-def input_data():
-    if request.method == 'POST':
-        ip_address = request.remote_addr
-        data = request.get_json()
-        insert_details(ip_address, 'data')
-        print(ip_address)
-        print(data)
-        #return render_template('inputdata.html', var=data)
-        return dumps(data)
-'''
 
 '''
 Inserts ip_address and raw data into database
@@ -107,14 +111,21 @@ def insert():
         '''
         return render_template('index.html',var=details)
 
-@application.route('/test', methods = ['GET', 'POST'])
-def test():
+'''
+Authenticates user data sent 
+'''
+@application.route('/', methods = ['GET', 'POST'])
+def index():
     if request.method == 'GET':
-        return render_template('testml.html')
+        detail = get_last_detail()
+        return render_template('results.html', var=detail)
     if request.method == 'POST':
-        
-        # Get data from site
-        data = request.form['data']
+        # Get data
+        raw_data = request.get_json()
+
+        # Parse data
+        ip_address = raw_data['ip_address']
+        data = raw_data['data']
 
         template = re.compile('ax(-?\d+)ay(-?\d+)az(-?\d+)gx(-?\d+)gy(-?\d+)gz(-?\d+)')
         readings = template.findall(data)
@@ -140,7 +151,13 @@ def test():
         #results = ax_array
         print(results)
 
-        return render_template('results.html', var=results)
+        if (results):
+            who = "Otto"
+        else:
+            who = "Hasaru"
+
+        insert_details('ip_address', who)
+        return render_template('results.html', var=who)
         # Test data with ML algorithm
         # TEST ML HERE
 
@@ -149,6 +166,6 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run(host='127.0.0.1', port=8013)
+    application.run(host='127.0.0.1', port=8011)
     #application.run()
     #application.run(host='0.0.0.0')
