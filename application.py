b2e229bfc42ee68c40a8b9ea635fce3ecfcdc382
Mike Gysel <z5251938@ad.unsw.edu.au>
2020-11-23 13:20:14 +1100

    elif update

--- application.py
+++ application.py
@@ -171,6 +171,6 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run(host='127.0.0.1', port=8017)
+    application.run(host='127.0.0.1', port=8018)
     #application.run()
     #application.run(host='0.0.0.0')
